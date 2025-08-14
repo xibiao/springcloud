1bddb9a9862f7117884e038d8c168dd630b7ca7d
yitianzhaohai <ytzh@qq.com>
2025-08-14 16:09:15 +0800

    从Nacos同步规则至Sentinel

--- services/service-order/src/main/java/com/springcloud/demo/order/OrderApplication.java
+++ services/service-order/src/main/java/com/springcloud/demo/order/OrderApplication.java
@@ -2,10 +2,12 @@ package com.springcloud.demo.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+// 由于该服务不需要连接数据库，所以排除数据源自动配置类，否则服务启动失败
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableDiscoveryClient
 @EnableFeignClients  // 开启feign的远程调用功能
 public class OrderApplication {
--- services/service-product/src/main/java/com/springcloud/demo/product/ProductApplication.java
+++ services/service-product/src/main/java/com/springcloud/demo/product/ProductApplication.java
@@ -2,9 +2,11 @@ package com.springcloud.demo.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+// 由于该服务不需要连接数据库，所以排除数据源自动配置类，否则服务启动失败
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableDiscoveryClient // 开启服务发现功能
 public class ProductApplication {
     public static void main(String[] args) {
